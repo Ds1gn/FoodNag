bca75eed23fa5489814e43477795ed70a8284228
Mike Finocchiaro <pushon@onajunket.com>
2015-05-21 16:02:07 -0400

    Trying to add field to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_devise_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_devise_parameters
+		devise_parameter_sanitizer.for(:account_update) << :first_name
+	end
+
 end
