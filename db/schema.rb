5800fecec460e21ab9e8ea1e0a99aceb79205f64
Dan Schur <dschur13@gmail.com>
2015-05-25 20:12:23 -0400

    slight changes to allow edits to tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521224809) do
+ActiveRecord::Schema.define(version: 20150522015703) do
 
   create_table "expirations", force: :cascade do |t|
     t.integer  "food_id"
@@ -67,12 +67,12 @@ ActiveRecord::Schema.define(version: 20150521224809) do
   add_index "purchases", ["user_id"], name: "index_purchases_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(version: 20150521224809) do
     t.string   "zip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
