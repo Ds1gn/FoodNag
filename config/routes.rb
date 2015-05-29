b8a66f1c114c05a3d57f8314246edca6251ad5b2
Dan Schur <dschur13@gmail.com>
2015-05-28 21:05:17 -0400

    displaying charts of food purchases by zipcode in advertisers zip page

--- config/routes.rb
+++ config/routes.rb
@@ -24,9 +24,6 @@ Rails.application.routes.draw do
 root :to => 'static_page#home'
   post 'results' => 'recipe#results'
 
-
-  root :to => 'static_page#home'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
