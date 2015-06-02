486e9da8d2936542f595b00481e5d048a1fe790d
Mike Finocchiaro <pushon@onajunket.com>
2015-06-01 21:19:30 -0400

    Recipe search api working

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -3,27 +3,34 @@ class RecipeController < ApplicationController
 
   def ingredient
     @purchases = Purchase.all
+    @food_categories = FoodCategory.all
   end
 
   def show_recipes
-  	@display = Recipe.get_recipes(params[:ingredient])
+    @display = Recipe.get_recipes(params[:ingredient])
 
-  	@ingredient_lines = @display["ingredientLines"]
+    @ingredient_lines = @display["ingredientLines"]
 
-  	@display2 = Recipe.get_recipes2(params[:ingredient1],params[:ingredient2])
-  	@ingredient_lines2 = @display2["ingredientLines"]
+    @display2 = Recipe.get_recipes2(params[:ingredient1],params[:ingredient2])
+    @ingredient_lines2 = @display2["ingredientLines"]
   end
 
-  
+  def results
+    @recipe_food = Recipe.when_searched(params[:ingredients])
 
+    @ingredient_lines = @recipe_food["ingredientLines"]
+
+    @recipe_food2 = Recipe.when_searched2(params[:ingredients1],params[:ingredients2])
+    @ingredient_lines2 = @recipe_food2["ingredientLines"]
+  end
  
   private
   def set_recipe
-  	Recipe.find(params[:id])
+    Recipe.find(params[:id])
   end
 
   def recipe_params
-  	params.require(:recipe).permit(:recipe_title, :recipe_label, :recipe_image, :recipe_url, :recipe_ingredient)
+    params.require(:recipe).permit(:recipe_title, :recipe_label, :recipe_image, :recipe_url, :recipe_ingredient)
   end
 
 end
