c16f51659415ab1f69aa8ecca1f305e49e98294b
Mike Finocchiaro <pushon@onajunket.com>
2015-06-01 23:18:57 -0400

    Menu begins collapsed

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,21 +1,10 @@
 $(function() {
 
 
-	
+
 	$('.button').click(function(){
-			console.log('fruits')
-		$('body').toggleClass('open');
+		$('section').toggleClass('open');
 	});
 
-
-
-	// $('.nav-toggle').on('click', function(){
-	// 	$('.wrapper').toggleClass('open');
-	// });
-
-	// $('.button-fruit').click(function(){
-	// 	$('body .nav-fruit').toggleClass('open');
-	// 	console.log('fruits');
-	// });	
 
 });
